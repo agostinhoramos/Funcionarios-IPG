19f1e4246d4270b10b7b636a11c77f8b5def3e04
agostinhopina95 <agostinhopina95@gmail.com>
2019-11-19 14:46:55 +0000

    initial commit

---
+++ IPG Funcionarios/Models/Funcionario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPG_Funcionarios.Models
+{
+    public class Funcionario
+    {
+        public int idFuncionario { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Telefone { get; set; }
+    }
+}
