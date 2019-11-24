782c45e23ff8224f3927aa10a07d46d1181859d0
agostinhopina95 <agostinhopina95@gmail.com>
2019-11-24 20:57:03 +0000

    Criação de layout.

--- IPG Funcionarios/Models/Funcionario.cs
+++ IPG Funcionarios/Models/Funcionario.cs
@@ -7,10 +7,6 @@ namespace IPG_Funcionarios.Models
 {
     public class Funcionario
     {
-        public int idFuncionario { get; set; }
-
-        public string Nome { get; set; }
-
-        public string Telefone { get; set; }
+        
     }
 }
---
+++ Projecto Final/README.txt
@@ -0,0 +1,7 @@
+https://github.com/agostinhopina95/Funcionarios-IPG
+
+BDII_1012444
+bdii
+test
+bd.ipg.pt
+1521
