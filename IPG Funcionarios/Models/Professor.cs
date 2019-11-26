1c439204112ce3ac22e82e5e14f40294b9681141
agostinhopina95 <agostinhopina95@gmail.com>
2019-11-26 23:23:42 +0000

    Criação de Views e Modelos parte 1

---
+++ IPG Funcionarios/Models/Professor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPG_Funcionarios.Models
+{
+    public class Professor
+    {
+        public int ProfessorId { get; set; }
+
+        [Required(ErrorMessage = "Por favor, introduza o nome do professor.")]
+        [RegularExpression(@"([A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ\s]+)", ErrorMessage = "Nome inválido")]
+        [StringLength(maximumLength: 50, MinimumLength = 3)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Por favor, introduza o seu número de telemóvel/telefone")]
+        [RegularExpression(@"(2\d{8})|(9[1236]\d{7})", ErrorMessage = "Contacto inválido")]
+        public string NumeroTelemovel { get; set; }
+
+        [Required(ErrorMessage = "Por favor, introduza o email")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+
+        public string Gabinete { get; set; }
+    }
+}
