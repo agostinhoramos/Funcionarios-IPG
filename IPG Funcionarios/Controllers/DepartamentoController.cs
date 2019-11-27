2f3c9495cf3c58c2742d0db92cf6ebb33d3afcee
VircaG <vircagoncalves39@gmail.com>
2019-11-27 12:29:53 +0000

    Criação de Controlador para Departamento

---
+++ IPG Funcionarios/Controllers/DepartamentoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IPG_Funcionarios.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
