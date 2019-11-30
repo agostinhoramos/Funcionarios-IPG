2732236116d950bcb3c3c3a66db82ea56d509f63
Soila Benguela <1701061@sal.ipg.pt>
2019-11-30 00:22:16 +0000

    Criação do Modelo Funcionarios e os seus campos.

---
+++ IPG Funcionarios/Models/Funcionarios.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPG_Funcionarios.Models {
+    public class Funcionarios
+
+        {
+    public int FuncionariosId { get; set; }
+
+    [Required(ErrorMessage = "Por favor, digite o nome do Funcionarios")]
+    [RegularExpression(@"([A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ\s]+)", ErrorMessage = "Nome inválido")]
+    [StringLength(maximumLength: 50, MinimumLength = 3)]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "Por favor, digite o número do telefone")]
+    [RegularExpression(@"(2\d{8})|(9[1236]\d{7})", ErrorMessage = "Número inválido")]
+    public string Telemovel { get; set; }
+
+    [Required(ErrorMessage = "Por favor, digite o número do telefone") ]
+    [RegularExpression(@"(2\d{8})|(9[1236]\d{7})", ErrorMessage = "Número inválido")]
+    public string Telefone { get; set; }
+
+    [Required(ErrorMessage = "Por favor, introduza o email")]
+    [EmailAddress(ErrorMessage = "Email inválido")]
+    public string Email { get; set; }
+
+    public string Horas_de_trabalho { get; set; }
+
+
+}
+}
