c9eaa42a74a3c85da1c9ad84896240e763f298a3
VircaG <vircagoncalves39@gmail.com>
2019-12-03 14:51:13 +0000

    Eliminei e voltei a criar o  controlador e view para Departamento

--- IPG Funcionarios/Data/IPGFuncionariosDbContext.cs
+++ IPG Funcionarios/Data/IPGFuncionariosDbContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using IPG_Funcionarios.Models;
 
 namespace IPG_Funcionarios.Data {
     public class IPGFuncionariosDbContext : IdentityDbContext {
         public IPGFuncionariosDbContext(DbContextOptions<IPGFuncionariosDbContext> options)
             : base(options) {
         }
+        public DbSet<IPG_Funcionarios.Models.Departamento> Departamento { get; set; }
     }
 }
