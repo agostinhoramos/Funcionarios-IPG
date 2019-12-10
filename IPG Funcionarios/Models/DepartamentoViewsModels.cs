ac99c856dfb02e32ae57479960574ec87a02f93a
VircaG <vircagoncalves39@gmail.com>
2019-12-10 15:26:45 +0000

    Criação do Modelo para Paginação

---
+++ IPG Funcionarios/Models/DepartamentoViewsModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPG_Funcionarios.Models
+{
+    public class DepartamentoViewsModels
+    {
+        public IEnumerable <Departamento> departamentos {get; set;}
+        public int PaginaCorrente {get; set;}
+        public int PaginaTotal {get; set;}
+        public int MostrarPrimeiraPagina {get; set;}
+        public  int MostrarUltimaPagina {get; set;}
+    }
+}
