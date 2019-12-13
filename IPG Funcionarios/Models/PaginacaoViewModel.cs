67bbae3895e588c62221596fe4b1d623ccfd04ae
Soila Benguela <1701061@sal.ipg.pt>
2019-12-13 21:47:15 +0000

    Criação do Modelo PaginacaoViewModel com a pasta  Paginacao  e a sua respectiva class com o mesmo nome.

---
+++ IPG Funcionarios/Models/PaginacaoViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPG_Funcionarios.Models
+{
+    public class PaginacaoViewModel
+    {
+    }
+}
---
+++ IPG Funcionarios/Paginacao/Paginacao.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPG_Funcionarios.Paginacao
+{
+    public class Paginacao :TagHelper
+
+    {
+    }
+}
