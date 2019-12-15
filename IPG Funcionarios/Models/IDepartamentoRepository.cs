2470f008cf163fbe62143acacde0669f2ee1d62a
VircaG <vircagoncalves39@gmail.com>
2019-12-15 11:48:03 +0000

    Criação da Interface para departamento

---
+++ IPG Funcionarios/Models/IDepartamentoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPG_Funcionarios.Models
+{
+    public interface IDepartamentoRepository
+     {
+        public IEnumerable<Departamento> Departamentos { get; }
+    }
+}
