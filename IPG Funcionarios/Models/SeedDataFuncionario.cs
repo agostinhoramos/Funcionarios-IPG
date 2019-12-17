105dfc5d80e6de8b3113f020492e64808f4cb872
Soila Benguela <1701061@sal.ipg.pt>
2019-12-17 12:17:34 +0000

    Mundaçao do Index do Modelo Funcionario, deixando o Layout do mesmo parecio com o dos outras , aumento do tamanha da pagina para 10 no FuncionarioController e a criação de campos fectícios  na Base de dados com o SeedDataFuncionario  .

--- IPG Funcionarios/Models/SeedDataFuncionario.cs
+++ IPG Funcionarios/Models/SeedDataFuncionario.cs
@@ -7,5 +7,20 @@ namespace IPG_Funcionarios.Models
 {
     public class SeedDataFuncionario
     {
+        public static void Populate(IPGFuncionariosDbContext db)
+        {
+            SeedFuncionario(db);
+
+        }
+        private static void SeedFuncionario(IPGFuncionariosDbContext db) {
+            if (db.Funcionario.Any()) return;
+
+            db.Funcionario.AddRange(
+                new Funcionario { Nome = "Lina Sousa", Telefone = "234567890", Email= "lina@gmail.com", Genero = "F", Morada = "Rua Xanana Gusmão " },
+                new Funcionario { Nome = "Lara Lima", Telefone = "912344567", Email = "lima@gmail.com", Genero = "F", Morada = "Rua Páiva" },
+                new Funcionario { Nome = "João Rita", Telefone = "923456211", Email = "joaorita@gmail.com", Genero = "m", Morada = "Rua da Alegria " }
+                );
+            db.SaveChanges();
+        }
     }
 }
