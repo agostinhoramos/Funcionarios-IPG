1e7be95898451250c903fa6ccd0d5575f7d12c7d
agostinhopina95 <agostinhopina95@gmail.com>
2019-12-25 13:30:40 +0000

    Criação de Modelo Login. Ajustes na views do Professor, Página Inicial, Algumas mudanças do Layout Geral.

--- IPG Funcionarios/Data/ApplicationDbContext.cs
+++ IPG Funcionarios/Data/ApplicationDbContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using IPG_Funcionarios.Models;
 
 namespace IPG_Funcionarios.Data {
     public class ApplicationDbContext : IdentityDbContext {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options) {
         }
+        public DbSet<IPG_Funcionarios.Models.UserAccount> UserAccount { get; set; }
     }
 }
