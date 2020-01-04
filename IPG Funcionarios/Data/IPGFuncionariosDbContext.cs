6e6796f236a600375f9cb74a3342d15926f344fc
Agostinho P. Ramos <agostinhopina95@gmail.com>
2020-01-04 12:24:21 +0000

    Scaffolding do "Servico".

--- IPG Funcionarios/Data/IPGFuncionariosDbContext.cs
+++ IPG Funcionarios/Data/IPGFuncionariosDbContext.cs
@@ -22,5 +22,6 @@ namespace IPG_Funcionarios.Models
         public DbSet<IPG_Funcionarios.Models.Professor> Professor { get; set; }
         public DbSet<IPG_Funcionarios.Models.Departamento> Departamento { get; set; }
         public DbSet<IPG_Funcionarios.Models.Funcionario> Funcionario { get; set; }
+        public DbSet<IPG_Funcionarios.Models.Servico> Servico { get; set; }
     }
 }
