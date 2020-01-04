55590b5bbacd61a96690636219da0cf5ecbf68b9
Agostinho P. Ramos <agostinhopina95@gmail.com>
2020-01-04 12:06:37 +0000

    Criação do modelo "Cargo".

---
+++ IPG Funcionarios/Models/Cargo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace IPG_Funcionarios.Models
+{
+    public class Cargo
+    {
+        public int CargoID { get; set; }
+        [Required(ErrorMessage = "Por favor, insira um nome de cargo")]
+        [StringLength(250)]
+        [Display(Name = "Nome")]
+        public string NomeCargo { get; set; }
+    }
+}
