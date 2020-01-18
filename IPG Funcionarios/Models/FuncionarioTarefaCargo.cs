f68a4e028e0640f6c20bb8d441bc20b9457a5fda
Agostinho Pina Ramos <agostinhopina95@gmail.com>
2020-01-18 22:06:35 +0000

    Criação de novo modelo "FuncionarioTarefaCargo" & "ProfessorTarefaCargo".

---
+++ IPG Funcionarios/Models/FuncionarioTarefaCargo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPG_Funcionarios.Models
+{
+    public class FuncionarioTarefaCargo
+    {
+        public int FuncionarioForeignKey { get; set; }
+        public int TarefaForeignKey { get; set; }
+        public int CargoForeignKey { get; set; }
+    }
+}
---
+++ IPG Funcionarios/Models/ProfessorTarefaCargo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPG_Funcionarios.Models
+{
+    public class ProfessorTarefaCargo
+    {
+        public int ProfessorForeignKey { get; set; }
+        public int TarefaForeignKey { get; set; }
+        public int CargoForeignKey { get; set; }
+
+    }
+}
